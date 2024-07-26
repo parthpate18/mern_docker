b25a1c2975593abaf4a71b1baacdd2b590b45332
shivangkpatel <shivang02052005@gmail.com>
2024-07-26 21:35:10 +0530

    Changes In Docker File

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -10,7 +10,7 @@ function HomePage() {
 
   useEffect(() => {
     axios
-      .get(import.meta.env.VITE_API_PATH + '/api/posts')
+      .get('http://localhost:5000'+ '/api/posts')
       .then((response) => {
         setPosts(response.data);
       })
